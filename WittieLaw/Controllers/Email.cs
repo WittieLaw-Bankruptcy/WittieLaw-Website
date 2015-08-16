b04ed3ec342ce8a984a44eb47fb42b7f936227af
Kobie Williams <info@cobysolutions.com>
2015-08-16 13:39:58 -0500

    Wittie Law Bankruptcy Website

---
+++ WittieLaw/Controllers/Email.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Linq;
+
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
+
+namespace WittieLaw.Controllers
+{
+    public class Email
+    {
+        public static void sendmail(string emailmessage,string fullname,string emailaddress)
+        {
+            MailDefinition message = new MailDefinition();
+            MailMessage ConfirmationMail = new MailMessage();
+            String sFrom = "info@wittielaw.com ";
+            String sTo = "info@wittielaw.com ";
+            sTo = sTo.Trim();
+            ConfirmationMail.From = new MailAddress(sFrom);
+          
+            ConfirmationMail.To.Add(sTo);
+            if (emailmessage !=string.Empty)
+            {
+                emailmessage="Visitor Name: " + fullname + "\n" + "Message: " + emailmessage;
+            ConfirmationMail.Body = emailmessage;
+            }else
+            {
+                emailmessage="Visitor Name: " + fullname + "\n" + "Message: Visitor left no message available.";
+                ConfirmationMail.Body = emailmessage;
+
+            }
+                message.IsBodyHtml = true;
+            ConfirmationMail.Subject = "New Inquiry";
+            SmtpClient Client = new SmtpClient("smtpout.secureserver.net", 25);
+            //SmtpClient Client = new SmtpClient("dedrelay.secureserver.net", 25);
+            Client.Credentials = new NetworkCredential("info@wittielaw.com", "abc123");
+            Client.Send(ConfirmationMail);
+            return;
+        }
+    }
+}
---
+++ WittieLaw/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WittieLaw.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Bankruptcy()
+        {
+
+            return View();
+        }
+        public ActionResult Courses()
+        {
+            return View();
+        }
+        public ActionResult FAQ()
+        {
+            return View();
+        }
+      
+        public ActionResult Contact(FormCollection inquiry)
+        {
+            string fullname = string.Empty;
+            string email = string.Empty;
+            string message = string.Empty;
+             fullname = inquiry["name"];
+             email = inquiry["emailaddress"];
+            message = inquiry["message"];
+            if(!(fullname =="" || fullname==null) && !(email=="" || email==null))
+            Email.sendmail(message, fullname, email.Trim());
+            
+            return View();
+        }
+
+    }
+}
